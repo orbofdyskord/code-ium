b830193d32c56e2e9c1fdcc0f2af38fff23a45fd
sofzia <67490977+sofzia@users.noreply.github.com>
2022-05-21 20:52:35 -0400

    top taskbar stuff

--- taskbar.php
+++ taskbar.php
@@ -1,3 +1,8 @@
 <html>
-    <div></div>
+    <head>
+      <link rel="stylesheet" href="style.css">
+    </head>
+    <div class="topnav">
+      <a href= "settings.php"><img src="pics/setting-icon.png"></a>
+    </div>
 </html>
